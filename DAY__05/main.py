d8884fb6916c799ed7f6442693ad1d9c1dc65003
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-11-27 19:00:28 +0600

    first commit

---
+++ DAY__05/main.py
@@ -0,0 +1,10 @@
+"""
+This is multiline comment syntax
+"""
+
+# This is single line comment syntax
+
+print("Hello World")
+print(3 + 5)
+print("3 + 5", 5 + 5, sep=" - ", end=" = ")
+print(10 + 10)
