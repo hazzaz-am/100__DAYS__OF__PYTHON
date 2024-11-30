d76447db8f3ed985ea88509e86085ec9a96cd09c
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-11-30 16:18:37 +0600

    day__06-data-types completed

---
+++ DAY__06/main.py
@@ -0,0 +1,19 @@
+st = "Hazzaz"
+nu = 34
+lis = [1, 2, ["amin", 2]]
+tup = (3, 4, (4, 5, "huzzat"))
+dic = {"name": "hazzaz", "age": 45, "isLogged": True, "social_profile":{
+  "isProductive": False
+}}
+
+print(st)
+print(nu)
+print(lis)
+print(tup)
+print(dic)
+
+print(type(st))
+print(type(nu))
+print(type(lis))
+print(type(tup))
+print(type(dic))
