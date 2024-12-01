50199cc9da773f5b9334bf1486c9b4b443353dac
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-01 14:12:48 +0600

    day-10 input function practise

---
+++ DAY__10/main.py
@@ -0,0 +1,9 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("a + b is: ", int(a) + int(b))
+print("a - b is: ", int(a) - int(b))
+print("a * b is: ", int(a) * int(b))
+print("a / b is: ", int(a) / int(b))
+print("a // b is: ", int(a) // int(b))
+print("a % b is: ", int(a) % int(b))
