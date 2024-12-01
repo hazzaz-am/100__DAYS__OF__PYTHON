051569dbbb1d0d39528ccde340e85f4a9a9d2872
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-01 14:50:04 +0600

    day-11- string access

---
+++ DAY__11/main.py
@@ -0,0 +1,31 @@
+name = "hello"
+nam = 'hi'
+na = """Hello, My name is Hazzaz.
+I am from Dhaka and currently I am learning Python language and trying to improve my knowledge in Islam."""
+
+print(name)
+print(nam)
+print(na)
+
+doble_quote = "I'm 23 years old."
+print(doble_quote)
+
+escape_quote = 'He\'s my love'
+print(escape_quote)
+
+
+# access characters
+print(name[0]) # h
+print(name[-0]) # h
+print(name[-1]) # o
+print(name[-2]) # l
+
+# access characters using loop
+
+for char in name:
+  print(char)
+  
+print("---------------")
+
+for char in nam:
+  print(char)
