fa42622071da5b73ee1366402ba6e9868c6ab2ce
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-03 14:18:17 +0600

    practise for loop

---
+++ DAY__17/main.py
@@ -0,0 +1,23 @@
+colors = ["Red", "Green", "Blue"]
+
+for color in colors:
+  print(color)
+  print("Spelling of", color)
+  for clr in color:
+    print(clr)
+  print("-------")
+
+
+for num in range(10):
+  print(num)
+
+
+for n in range(1, 11):
+  print(n)
+
+for odd in range(1, 20, 2):
+  print(odd)
+
+for even in range(0, 20, 2):
+  print(even)
+
